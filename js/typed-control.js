46a2c158ef58f27fb87753235e475533fc9c6d3c
banglinhn <140496631+banglinhn@users.noreply.github.com>
2023-07-25 16:11:04 +0700

    Add files via upload

---
+++ js/typed-control.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+strings: ["Web Design.", "Code Develop.","Apps Design.","World Create."],
+typeSpeed:35,
+backSpeed:35,
+loop:true,
+});
+$('.counter').countUp();
+
+new WOW().init();
